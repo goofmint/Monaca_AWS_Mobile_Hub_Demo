1ae60635d39d9b60b06ae655a8983a7f7143e8c9
Atsushi Nakatsugawa <atsushi@moongift.jp>
2018-04-04 16:59:16 +0900

    Add Storage feature.

--- www/src/app.js
+++ www/src/app.js
@@ -1,7 +1,10 @@
-import Amplify, { Auth, Analytics } from 'aws-amplify';
+import Amplify, { Auth, Storage } from 'aws-amplify';
 import aws_exports from './aws-exports.js';
 Amplify.configure(aws_exports);
 
 ons.ready(function() {
   window.Auth = Auth;
+  console.log(Auth);
+  console.log(Storage)
+  window.Storage = Storage;
 });
