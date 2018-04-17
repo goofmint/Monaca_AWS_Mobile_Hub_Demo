64be26f32ad7a24a03c7b7ef3913762cae206cef
Atsushi Nakatsugawa <atsushi@moongift.jp>
2018-04-17 17:37:09 +0900

    Add database

--- www/src/app.js
+++ www/src/app.js
@@ -1,10 +1,10 @@
-import Amplify, { Auth, Storage } from 'aws-amplify';
+import Amplify, { Auth, Storage, API } from 'aws-amplify';
 import aws_exports from './aws-exports.js';
 Amplify.configure(aws_exports);
 
 ons.ready(function() {
   window.Auth = Auth;
-  console.log(Auth);
-  console.log(Storage)
   window.Storage = Storage;
+  console.log(API)
+  window.API = API;
 });
