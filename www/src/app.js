88db17e994f625824ab327214a8c166266c6a70a
Atsushi Nakatsugawa <atsushi@moongift.jp>
2018-04-18 13:55:36 +0900

    Add analytics

--- www/src/app.js
+++ www/src/app.js
@@ -1,10 +1,11 @@
-import Amplify, { Auth, Storage, API } from 'aws-amplify';
+import Amplify, { Auth, Storage, API, Analytics } from 'aws-amplify';
 import aws_exports from './aws-exports.js';
 Amplify.configure(aws_exports);
 
 ons.ready(function() {
   window.Auth = Auth;
   window.Storage = Storage;
+  window.Analytics = Analytics;
   console.log(API)
   window.API = API;
 });
