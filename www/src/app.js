98097cb51794752a24efacffe41e963ba6a1ce8a
Atsushi <atsushi@moongift.jp>
2018-08-05 18:21:17 +0900

    Debug message

--- www/src/app.js
+++ www/src/app.js
@@ -2,6 +2,7 @@ import Amplify, { Auth, Storage, API, Analytics } from 'aws-amplify';
 import aws_exports from './aws-exports.js';
 Amplify.configure(aws_exports);
 
+console.log(Auth);
 ons.ready(function() {
   window.Auth = Auth;
   window.Storage = Storage;
