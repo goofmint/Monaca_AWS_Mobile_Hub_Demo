3fea1c1b20f445c3f548325699a0df0195395f53
Atsushi <atsushi@moongift.jp>
2018-08-15 18:34:34 +0900

    Delete database dna logic

--- www/src/app.js
+++ www/src/app.js
@@ -1,4 +1,4 @@
-import Amplify, { Auth, Storage, API, Analytics } from 'aws-amplify';
+import Amplify, { Auth, Storage, Analytics } from 'aws-amplify';
 import aws_exports from './aws-exports.js';
 Amplify.configure(aws_exports);
 
@@ -7,6 +7,4 @@ ons.ready(function() {
   window.Auth = Auth;
   window.Storage = Storage;
   window.Analytics = Analytics;
-  console.log(API)
-  window.API = API;
 });
